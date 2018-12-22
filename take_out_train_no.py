5c799eeb7afc65e3f975485d00036695ee6895b1
faulkhuang <faulkhuang@hotmail.com>
2018-12-22 09:10:17 +0800

    Create venv enviroment.

--- take_out_train_no.py
+++ take_out_train_no.py
@@ -6,7 +6,7 @@ import xlrd
 #reload(sys)
 #sys.setdefaultencoding('utf8')
 
-book = xlrd.open_workbook('Timetable.xls')
+book = xlrd.open_workbook('AllStationTimeTable.xls')
 sheet = book.sheet_by_index(0)
 print(sheet.ncols)
 print(sheet.nrows)
