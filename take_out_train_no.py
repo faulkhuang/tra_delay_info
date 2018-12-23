f0248f0959c08c5ec7aad1bfb1c46167507dfdcd
faulkhuang <faulkhuang@hotmail.com>
2018-12-23 09:02:56 +0800

    take_out_train_no.py modify.

--- take_out_train_no.py
+++ take_out_train_no.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#from __future__ import print_fuction
-#import sys
 import xlrd
-#reload(sys)
-#sys.setdefaultencoding('utf8')
 
 book = xlrd.open_workbook('Timetable.xls')
 sheet = book.sheet_by_index(0)
@@ -15,8 +11,7 @@ nrows = sheet.nrows
 
 for row in range(nrows):
     for col in range(ncols):
-        if isinstance(sheet.cell_value(row, col), int):
-            train_no = sheet.cell_value(row, col)
-            print(train_no)
-
+        print(sheet.cell_value(col,row))
+    
+            
 #if __name__ == '__main__':
