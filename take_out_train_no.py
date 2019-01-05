a4db4b52062331c9092e51ae5909420c297d964d
faulkhuang <faulkhuang@hotmail.com>
2019-01-06 02:35:06 +0800

    get train no sucess.

--- take_out_train_no.py
+++ take_out_train_no.py
@@ -1,24 +1,28 @@
 # -*- coding: utf-8 -*-
-
+import os
+import pandas
 import xlrd
 
-book = xlrd.open_workbook('AllStationTimeTable.xls')
+book = xlrd.open_workbook('AllStationTimeTable.xls', logfile=open(os.devnull, 'w'))
+#pd.read_excel(book)
 sheet = book.sheet_by_index(0)
 print(sheet.ncols)
 print(sheet.nrows)
 ncols = sheet.ncols
 nrows = sheet.nrows
 
-for row in range(nrows):
-    for col in range(ncols):
-        print(sheet.cell_value(col,row))
+for col in range(ncols):
+    for row in range(nrows):
         cell_value = sheet.cell_value(row, col)
-        if int(cell_value) == cell_value:
-            
-#        if cell.ctype in (2,3) and int(cell_value) == cell_value:
-#            cell_value = int(cell_value)
-#            print(cell_value)
+        if not cell_value:
+            pass
+        else:
+            try:
+                int(cell_value)
+                print(cell_value)
+            except:
+                pass
 #        else:
-
+#            pass
 
 #if __name__ == '__main__':
